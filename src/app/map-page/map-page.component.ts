ccbb6306d6b998d686c23aba6e366dfb07c9afe7
Ilan Kundih <ilankundih@Ilans-iMac.local>
2023-12-05 22:04:34 +0100

    added mapbox-directions

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import * as MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 
 @Component({
   selector: 'app-map-page',
   templateUrl: './map-page.component.html',
-  styleUrls: ['./map-page.component.css']
+  styleUrls: ['./map-page.component.css'],
 })
 export class MapPageComponent implements OnInit {
-
   ngOnInit(): void {
-    (mapboxgl as typeof mapboxgl).accessToken = 'pk.eyJ1IjoiaWxhbmt1bmRpaCIsImEiOiJjbHA4Ymh6OXkyd21lMnZxa3lqdnZqMDJjIn0.enGCVPw4Xlq_IGo9qLfVuQ';
+    (mapboxgl as typeof mapboxgl).accessToken =
+      'pk.eyJ1IjoiaWxhbmt1bmRpaCIsImEiOiJjbHA4Ymh6OXkyd21lMnZxa3lqdnZqMDJjIn0.enGCVPw4Xlq_IGo9qLfVuQ';
     const map = new mapboxgl.Map({
       container: 'map-container', // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL
       center: [15.9, 45.8], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: 13, // starting zoom
     });
 
+    map.addControl(
+      new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+      }),
+      'top-left',
+    );
   }
 }
